50e65576535c9d0468dc03727b7d32100b80f3bd
TheKost_ <thekost2@yandex.ru>
2023-04-20 20:02:06 +0300

    v1.0.0

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -39,12 +39,12 @@ function Drawer({ onClose, onRemove, items = [], opened }) {
     <div className={`${styles.overlay} ${opened ? styles.overlayVisible : ''}`}>
       <div className={styles.drawer}>
         <h2 className="d-flex justify-between mb-30">
-          Корзина <img onClick={onClose} className="cu-p" src="img/btn-remove.svg" alt="Close" />
+          Корзина <img onClick={onClose} className="cu-p" src="/img/btn-remove.svg" alt="Close" />
         </h2>
 
         {items.length > 0 ? (
-          <div className="d-flex flex-column flex">
-            <div className="items flex">
+          <>
+            <div className={`flex ${styles.items}`}>
               {items.map((obj) => (
                 <div key={obj.id} className="cartItem d-flex align-center mb-20">
                   <div
@@ -58,7 +58,7 @@ function Drawer({ onClose, onRemove, items = [], opened }) {
                   <img
                     onClick={() => onRemove(obj.id)}
                     className="removeBtn"
-                    src="img/btn-remove.svg"
+                    src="/img/btn-remove.svg"
                     alt="Remove"
                   />
                 </div>
@@ -78,10 +78,10 @@ function Drawer({ onClose, onRemove, items = [], opened }) {
                 </li>
               </ul>
               <button disabled={isLoading} onClick={onClickOrder} className="greenButton">
-                Оформить заказ <img src="img/arrow.svg" alt="Arrow" />
+                Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
               </button>
             </div>
-          </div>
+          </>
         ) : (
           <Info
             title={isOrderComplete ? 'Заказ оформлен!' : 'Корзина пустая'}
